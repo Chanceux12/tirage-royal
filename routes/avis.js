6ab56438647f8fccef14f68042593f72690e6f21
Administrateur <tirageroyal033@gmail.com>
2025-10-17 03:07:47 +0200

    Sécurisation des routes admin des avis avec isAdmin

--- routes/avis.js
+++ routes/avis.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const avisController = require('../controllers/avisController');
-const { ensureAuthenticated, ensureAdmin } = require('../middlewares/auth');
+const { ensureAuthenticated } = require('../middlewares/auth');
+const { isAdmin } = require('../middlewares/authMiddleware');
 
+// 🚀 Routes publiques
 router.get('/', avisController.afficherAvis);
-router.post('/ajouter', avisController.ajouterAvis);
-router.get('/admin', ensureAuthenticated, ensureAdmin, avisController.afficherAdminAvis);
-router.post('/admin/:id/approuver', ensureAuthenticated, ensureAdmin, avisController.approuverAvis);
-router.post('/admin/:id/supprimer', ensureAuthenticated, ensureAdmin, avisController.supprimerAvis);
+router.post('/ajouter', ensureAuthenticated, avisController.ajouterAvis);
 
+// 🔐 Routes admin sécurisées
+router.get('/admin', ensureAuthenticated, isAdmin, avisController.afficherAdminAvis);
+router.post('/admin/:id/approuver', ensureAuthenticated, isAdmin, avisController.approuverAvis);
+router.post('/admin/:id/supprimer', ensureAuthenticated, isAdmin, avisController.supprimerAvis);
 
 module.exports = router;
